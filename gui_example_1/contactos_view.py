6472e0cdda2819d5ee60b26f692e3db28d5c3834
Angel <angel-primi@hotmail.com>
2022-12-01 15:36:35 -0600

    added ContactosForm and ContactoNuevo class

--- gui_example_1/contactos_view.py
+++ gui_example_1/contactos_view.py
@@ -2,6 +2,35 @@ import tkinter as tk
 import tkinter.messagebox as mb
 
 
+class ContactosForm(tk.LabelFrame):
+    campos = ('Last Name', 'First Name', 'Phone', 'Email')
+
+    def __init__(self, master, **kwargs):
+        super().__init__(master, text='Contacts', padx=10, pady=10, **kwargs)
+        self.frame = tk.Frame(self)
+        self.frame.pack()
+        self.entries = list(map(self.create_campo, enumerate(self.campos)))
+
+    def create_campo(self, campo):
+        position, text = campo
+        label = tk.Label(self.frame, text=text)
+        entry = tk.Entry(self.frame)
+        label.grid(row=position, column=0, pady=10)
+        entry.grid(row=position, column=1, pady=10)
+        return entry
+
+
+class ContactoNuevo(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.form = ContactosForm(self)
+        self.form.pack(padx=10, pady=10)
+
+    def show(self):
+        self.grab_set()
+        self.wait_window()
+
+
 class ContactosView(tk.Tk):
     def __init__(self):
         super().__init__()
