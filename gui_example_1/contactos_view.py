c2f945133f1e9ae8487ca22f64645db3a308dd20
Angel <angel-primi@hotmail.com>
2022-12-06 13:50:33 -0600

    added ContactosList and ActualizarContacto

--- gui_example_1/contactos_view.py
+++ gui_example_1/contactos_view.py
@@ -2,6 +2,21 @@ import tkinter as tk
 import tkinter.messagebox as mb
 
 
+class ContactosList(tk.Frame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.list = tk.Listbox(self, **kwargs)
+        bar = tk.Scrollbar(self, command=self.list.yview)
+
+        self.list.config(yscrollcommand=bar.set)
+        bar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+    def insert(self, contacto, index=tk.END):
+        text = '{}, {}'.format(contacto.last_name, contacto.first_name)
+        self.list.insert(index, text)
+
+
 class ContactosForm(tk.LabelFrame):
     campos = ('Last Name', 'First Name', 'Phone', 'Email')
 
@@ -37,6 +52,16 @@ class ContactoNuevo(tk.Toplevel):
         self.wait_window()
 
 
+class ActualizarContactosForm(ContactosForm):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.button_save = tk.Button(self, text='Guardar')
+        self.button_save.pack(side=tk.RIGHT, ipadx=5, padx=10, pady=10)
+
+        self.button_delete = tk.Button(self, text='Eliminar')
+        self.button_delete.pack(side=tk.RIGHT, ipadx=5, padx=10, pady=10)
+
+
 class ContactosView(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -46,6 +71,12 @@ class ContactosView(tk.Tk):
 
         self.button_new = tk.Button(self, text="Nuevo contacto")
         self.button_new.pack(side=tk.BOTTOM, pady=5)
+
+        self.list = ContactosList(self, height=20)
+        self.list.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.update_form = ActualizarContactosForm(self)
+        self.update_form.pack(side=tk.RIGHT, padx=10, pady=10)
 
     def set_controller(self, controller):
         self.button_new.config(command=controller.create_contact)
